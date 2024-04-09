45605ec6976bb951d3c9ccad91d4277874989a1f
unknown <muhammadali30804@gmail.com>
2024-04-09 07:05:26 +0500

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,28 +1,11 @@
 "use client"
 import MeetingTypeList from '@/components/MeetingTypeList';
-import getTime from '@/components/getTime';
-import getDate from '@/components/getDate';
 
 const Home = () => {
-  // const now = new Date();
+  const now = new Date();
 
-  // const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  // const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
-
-  // Get the current date and time from the client's system
-  // const now = new Date();
-
-  // Calculate the client's time zone offset in minutes
-  // const timeZoneOffsetMinutes = now.getTimezoneOffset();
-
-  // Create a new date object adjusted by the time zone offset
-  // const clientDateTime = new Date(now.getTime() - timeZoneOffsetMinutes);
-
-  // Format the time in 12-hour format with leading zeros (e.g., "hh:mm AM/PM")
-  // const time = clientDateTime.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-
-  // Format the full date in the client's locale
-  // const date = clientDateTime.toLocaleDateString('en-US', { dateStyle: 'full' });
+  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
@@ -32,8 +15,8 @@ const Home = () => {
             Upcoming Meeting at: 12:30 PM
           </h2>
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">{getTime()}</h1>
-            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{getDate()}</p>
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
       </div>
